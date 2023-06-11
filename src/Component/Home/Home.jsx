14378993ab7083c7db6e1ce44e177801042ec290
Maruf <marufsayfiyev483@gmail.com>
2023-06-11 23:10:28 +0500

    swiper

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,6 +1,9 @@
 import { Box, Button, Typography } from '@mui/material';
 import React from 'react';
 import tranck from "../images/Home/tranck.png"
+import xarita from "../images/Xarita/xarita.png"
+import moskva from "../images/Xarita/moskva.png"
+import moskva1 from "../images/Xarita/moskva1.png"
 
 const Home = () => {
     return (
@@ -18,11 +21,31 @@ const Home = () => {
             </Box>
             <img style={{width: '100%',height: '100%'}} src={tranck} alt="" />
            </Box>
-           <Box display={'flex'} justifyContent={'center'}>
-             <Box width={'86%'} border={1}> 
-                <Box></Box>
-                <Box></Box>
+           <Box display={'flex'} justifyContent={'center'}mt={15}>
+             <Box sx={{backgroundColor: '#FFFFFF',display: 'flex',justifyContent: 'center',alignItems: 'center',padding: '50px'}} width={'93%'}> 
+                <Box width={'45%'}>
+                    <Typography variant='h6' style={{fontFamily: 'Inter',fontWeight: '700',fontSize: '30px',lineHeight: '38px',color: '#23262F'}}>О нас</Typography>
+                    <Typography style={{fontFamily: 'Inter',fontWeight: '400',fontSize: '15px',lineHeight: '24px',color: '#72777A',order: '1'}}>Мы - MarcosLogistics, транспортно-экспедиционная <br /> компания, которая перевезет Ваши  грузы в кратчайшие <br /> сроки по принципу «от-двери-до-двери».</Typography>
+                    <br /><br />
+                    <Typography style={{fontFamily: 'Inter',fontWeight: '400',fontSize: '15px',lineHeight: '24px',color: '#72777A',order: '1'}}>Мы поможем Вам развивать бизнес или просто <br /> отправить письмо близкому человеку по доступным <br /> ценам. <br /><br />Мы ценим Ваше время!</Typography>
+                </Box>
+                <Box width={'61%'} position={'relative'}>
+                    <Box mt={5} ml={7} className="Москва" sx={{display: 'flex',justifyContent: 'center',alignItems: 'center',flexDirection: 'column',padding: '30px',position: 'absolute'}}>
+                     <img src={moskva} alt="" />
+                     <Typography style={{fontFamily: 'Inter',fontWeight: '500',fontSize: '16px',lineHeight: '30px',color: '#23262F'}}>Москва</Typography>
+                     <Typography style={{fontFamily: 'Inter',fontWeight: '400',fontSize: '12px',lineHeight: '30px',color: '#777E90'}}>За 7 дней</Typography>
+                    </Box>
+                    <Box ml={39} mt={25} className="Москва" sx={{display: 'flex',justifyContent: 'center',alignItems: 'center',flexDirection: 'column',padding: '30px',position: 'absolute'}}>
+                     <img src={moskva1} alt="" />
+                     <Typography style={{fontFamily: 'Inter',fontWeight: '500',fontSize: '16px',lineHeight: '30px',color: '#23262F'}}>Москва</Typography>
+                     <Typography style={{fontFamily: 'Inter',fontWeight: '400',fontSize: '12px',lineHeight: '30px',color: '#777E90'}}>За 7 дней</Typography>
+                    </Box>
+                    <img width={'100%'} height={'400px'} src={xarita} alt="" />
+                </Box>
              </Box>
+           </Box>
+           <Box>
+            
            </Box>
         </Box>
     );
