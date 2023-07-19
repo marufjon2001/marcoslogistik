bb0f8d5445e03f354906a0f0ccb1d5c1c8769daf
Frontent Developer <107357305+marufjon2001@users.noreply.github.com>
2023-07-19 21:01:40 +0500

    draver

--- src/Component/Lichniy/Lichniy.jsx
+++ src/Component/Lichniy/Lichniy.jsx
@@ -21,8 +21,8 @@ const Lichniy = () => {
     return (
         <Box>
             <Navbarwhite/>
-            <Box sx={{display: 'flex',gap: '30px'}}mt={5}>
-                <Box sx={{width: '17%',display: 'flex',justifyContent: "end"}} >
+            <Box  sx={{display: 'flex',gap: '30px'}}mt={5}>
+                <Box className="profil" sx={{width: '17%',display: 'flex',justifyContent: "end"}} >
                     <Box width={'60%'}>
                     <Typography onClick={menuitem} style={text ?{fontFamily: 'Inter',fontWeight: '700',fontSize: '14px',lineHeight: '80px',color: '#72777A',display: 'flex',alignItems: 'center',gap: '10px',cursor: 'pointer'}:{fontFamily: 'Inter',fontWeight: '700',fontSize: '14px',lineHeight: '80px',color: '#23262F',display: 'flex',alignItems: 'center',gap: '10px',cursor: 'pointer'}}><Person2SharpIcon/>Профиль</Typography>
                     <Typography onClick={menuitem} style={text1 ?{fontFamily: 'Inter',fontWeight: '700',fontSize: '14px',lineHeight: '16px',color: '#23262F',display: 'flex',alignItems: 'center',gap: '10px',cursor: 'pointer'}:{fontFamily: 'Inter',fontWeight: '700',fontSize: '14px',lineHeight: '16px',color: '#72777A',display: 'flex',alignItems: 'center',gap: '10px',cursor: 'pointer'}}><ManageHistorySharpIcon/>История <br /> отправлений</Typography>
@@ -33,31 +33,31 @@ const Lichniy = () => {
                 </Box>
                 {
                     text ?""
-                    :<Box sx={{width: '75%',display:"flex",flexWrap: 'wrap',gap: '20px'}}>
+                    :<Box className="lichniy_t" sx={{width: '75%',display:"flex",flexWrap: 'wrap',gap: '20px'}}>
                     <Box sx={{width: '48%',}}mt={2}>
-                        <Typography style={{fontFamily: 'Inter',fontWeight: '700',fontSize: '24px',lineHeight: '32px',color: '#23262F'}}>Личная информация</Typography><br />
+                        <Typography className='info' style={{fontFamily: 'Inter',fontWeight: '700',fontSize: '24px',lineHeight: '32px',color: '#23262F'}}>Личная информация</Typography><br />
                         <Typography style={{fontFamily: 'Inter',fontWeight: '500',fontSize: '14px',lineHeight: '20px',color: '#979C9E'}}>ФИО</Typography>
-                        <input type="text" style={{width: '100%',height: '40px',border: '2px solid #E3E5E5',fontFamily: 'Inter',fontWeight: '400',fontSize: '16px',lineHeight: '16px',color: 'black'}}/> <br /><br />
+                        <input className='inputtex' type="text" style={{width: '100%',height: '40px',border: '2px solid #E3E5E5',fontFamily: 'Inter',fontWeight: '400',fontSize: '16px',lineHeight: '16px',color: 'black'}}/> <br /><br />
                         <Typography style={{fontFamily: 'Inter',fontWeight: '500',fontSize: '14px',lineHeight: '20px',color: '#979C9E'}}>Номер телефона</Typography>
-                        <input type='number' style={{width: '100%',height: '40px',border: '2px solid #E3E5E5',fontFamily: 'Inter',fontWeight: '400',fontSize: '16px',lineHeight: '16px',color: 'black'}}/>
+                        <input className='inputtex' type='number' style={{width: '100%',height: '40px',border: '2px solid #E3E5E5',fontFamily: 'Inter',fontWeight: '400',fontSize: '16px',lineHeight: '16px',color: 'black'}}/>
                     </Box>
                     <Box sx={{width: '48%'}}mt={9}>
                         <Typography style={{fontFamily: 'Inter',fontWeight: '500',fontSize: '14px',lineHeight: '20px',color: '#979C9E'}}>ФИО</Typography>
-                        <input type="text" style={{width: '100%',height: '40px',border: '2px solid #E3E5E5',fontFamily: 'Inter',fontWeight: '400',fontSize: '16px',lineHeight: '16px',color: 'black'}}/> <br /><br />
+                        <input className='inputtex' type="text" style={{width: '100%',height: '40px',border: '2px solid #E3E5E5',fontFamily: 'Inter',fontWeight: '400',fontSize: '16px',lineHeight: '16px',color: 'black'}}/> <br /><br />
                         <Typography style={{fontFamily: 'Inter',fontWeight: '500',fontSize: '14px',lineHeight: '20px',color: '#979C9E'}}>Номер телефона</Typography>
-                        <input type='number' style={{width: '100%',height: '40px',border: '2px solid #E3E5E5',fontFamily: 'Inter',fontWeight: '400',fontSize: '16px',lineHeight: '16px',color: 'black'}}/>
+                        <input className='inputtex' type='number' style={{width: '100%',height: '40px',border: '2px solid #E3E5E5',fontFamily: 'Inter',fontWeight: '400',fontSize: '16px',lineHeight: '16px',color: 'black'}}/>
                     </Box>
-                    <hr style={{border: '1px solid black',width: '98%'}}/>
+                    <hr className='inputtex' style={{border: '1px solid black',width: '98%'}}/>
                     <Link style={{width: '100%'}} to={'/menu/Треккинг'}>
-                    <Button style={{fontWeight: '700',fontSize: '15px',lineHeight: '16px',color: 'white',backgroundColor: '#EA412B',height: '40px',width: '19%',padding: '10px'}}>Обновить данные</Button>
+                    <Button className='obnovitbtn' style={{fontWeight: '700',fontSize: '15px',lineHeight: '16px',color: 'white',backgroundColor: '#EA412B',height: '40px',width: '19%',padding: '10px'}}>Обновить данные</Button>
                     </Link>
                     <br></br><br />
                     </Box>
                 }
                 {
-                    text ?<Box sx={{width: '80%',position: 'relative'}}>
-                        <Typography style={{fontFamily: 'Inter',fontWeight: '700',fontSize: '24px',lineHeight: '70px',color: '#23262F'}}>История отправлений</Typography>
-                        <Box sx={{display: 'flex',gap: '10px',flexWrap: 'wrap'}}>
+                    text ?<Box className="istory" sx={{width: '80%',position: 'relative'}}>
+                        <Typography className="istory_tex" style={{fontFamily: 'Inter',fontWeight: '700',fontSize: '24px',lineHeight: '70px',color: '#23262F'}}>История отправлений</Typography>
+                        <Box className="novybtn" sx={{display: 'flex',gap: '10px',flexWrap: 'wrap'}}>
                             <Box>
                             <Button style={{fontFamily: 'Inter',fontWeight: '600',fontSize: '14px',lineHeight: '16px',color: '#EA412B',backgroundColor: '#FFF0F1',height: '40px',border: '2px solid #FFF0F1'}}>Новые (3)</Button><br /><br />
                              <Typography style={{fontFamily: 'Inter',fontWeight: '500',fontSize: '12px',lineHeight: '16px',color: '#72777A',display: 'flex',gap: '10px'}}>Номер заказа <img src={lichniy} alt="" /></Typography>
@@ -84,7 +84,7 @@ const Lichniy = () => {
                             </Box>
                         </Box>
                     <hr style={{border: '0.2px solid black',width: '98%'}}/>
-                    <Draver/>
+                    {/* <Draver/> */}
                     <br /><br /><br />
                     </Box>
                     :""
