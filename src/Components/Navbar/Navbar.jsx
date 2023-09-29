5c578f75551df8e85dcdee25b2d5ab88aec7713e
Frontent Developer <107357305+marufjon2001@users.noreply.github.com>
2023-09-29 17:17:13 +0500

    zaregist modal

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@ import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
+  
 
 const style = {
   position: "absolute",
